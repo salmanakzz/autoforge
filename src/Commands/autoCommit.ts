f566ad7559b4739dd354af5063b19494e9b1ffe9
salmanakzz <salmanakzz808@gmail.com>
2026-02-12 01:00:09 +0530

    `refactor(Commands/autoCommit): improve commit message formatting and error handling`

--- src/Commands/autoCommit.ts
+++ src/Commands/autoCommit.ts
@@ -4,24 +4,30 @@ import { generateCommit } from "../GitOps/generateCommit";
 
 export async function autoCommit() {
     const { msg, cwd } = await generateCommit();
+
+    const formattedMsg = `\`${msg}\``;
     const input = await vscode.window.showInputBox({
-        value: msg,
+        value: formattedMsg,
         prompt: "Edit your commit message",
     });
     if (input) {
-        exec(`git commit -m "${input}"`, { cwd }, (err, stdout, stderr) => {
-            if (err) {
-                const errorMessage =
-                    stderr?.trim() || stdout?.trim() || err.message;
-                vscode.window.showErrorMessage(
-                    `❌ Failed to commit message: ${errorMessage}`
-                );
-                return;
-            }
+        exec(
+            `git commit -m "${input.replace(/"/g, '\\"')}"`,
+            { cwd },
+            (err, stdout, stderr) => {
+                if (err) {
+                    const errorMessage =
+                        stderr?.trim() || stdout?.trim() || err.message;
+                    vscode.window.showErrorMessage(
+                        `❌ Failed to commit message: ${errorMessage}`,
+                    );
+                    return;
+                }
 
-            vscode.window.showInformationMessage(
-                `✅ Message commited: ${input}`
-            );
-        });
+                vscode.window.showInformationMessage(
+                    `✅ Message commited: ${input}`,
+                );
+            },
+        );
     }
 }
