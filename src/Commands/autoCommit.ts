80e4eceb6a23d15869dd1fef7564fc2f9239e939
salmanakzz <salmanakzz808@gmail.com>
2026-02-12 11:18:40 +0530

    refactor(Commands/autoCommit): simplify commit message handling and improve input sanitization

--- src/Commands/autoCommit.ts
+++ src/Commands/autoCommit.ts
@@ -4,30 +4,24 @@ import { generateCommit } from "../GitOps/generateCommit";
 
 export async function autoCommit() {
     const { msg, cwd } = await generateCommit();
-
-    const formattedMsg = `\`${msg}\``;
     const input = await vscode.window.showInputBox({
-        value: formattedMsg,
+        value: msg,
         prompt: "Edit your commit message",
     });
     if (input) {
-        exec(
-            `git commit -m "${input.replace(/"/g, '\\"')}"`,
-            { cwd },
-            (err, stdout, stderr) => {
-                if (err) {
-                    const errorMessage =
-                        stderr?.trim() || stdout?.trim() || err.message;
-                    vscode.window.showErrorMessage(
-                        `❌ Failed to commit message: ${errorMessage}`,
-                    );
-                    return;
-                }
-
-                vscode.window.showInformationMessage(
-                    `✅ Message commited: ${input}`,
+        exec(`git commit -m "${input}"`, { cwd }, (err, stdout, stderr) => {
+            if (err) {
+                const errorMessage =
+                    stderr?.trim() || stdout?.trim() || err.message;
+                vscode.window.showErrorMessage(
+                    `❌ Failed to commit message: ${errorMessage}`,
                 );
-            },
-        );
+                return;
+            }
+
+            vscode.window.showInformationMessage(
+                `✅ Message commited: ${input}`,
+            );
+        });
     }
 }
